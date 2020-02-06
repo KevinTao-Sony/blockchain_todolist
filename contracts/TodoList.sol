2ab7db07a7079fab977f4130f8631cdf668395db
Kevin Tao <47814475+KevinTao-Sony@users.noreply.github.com>
2020-02-06 14:06:39 +0000

    List task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,5 +4,21 @@ contract TodoList {
 
 	uint public taskCount = 0;
 
+	struct Task{
+		uint id;
+		string content;
+		bool completed;
+	}
+	///saving the task into the block chain
+	mapping(uint => Task) public tasks;
 
+	constructor() public {
+		createTask("first item");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount ++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+
+	}
 }
